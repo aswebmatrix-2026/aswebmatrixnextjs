83bc4134f6e23c415fdf203155e3af0fcd6b0a31
Anmol <sharmaanmol3585@gmail.com>
2026-03-14 07:57:55 +0530

    update robts

--- app/robots.ts
+++ app/robots.ts
@@ -1,24 +1,21 @@
-// app/robots.txt/route.ts
-import { NextResponse } from 'next/server'
+import { MetadataRoute } from 'next'
 
-export const dynamic = 'force-static'
-
-export async function GET() {
-  const robotsTxt = `User-agent: *
-Allow: /
-Disallow: /admin/
-Disallow: /api/
-Disallow: /private/
-Disallow: /*.json$
-Disallow: /*.xml$
-
-Sitemap: https://www.aswebmatrix.com/sitemap.xml
-Host: https://www.aswebmatrix.com
-`
-
-  return new NextResponse(robotsTxt, {
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-  })
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/admin/',
+          '/api/',
+          '/private/',
+          '/*.json$',
+          '/*.xml$'
+        ],
+      },
+    ],
+    sitemap: 'https://www.aswebmatrix.com/sitemap.xml',
+    host: 'https://www.aswebmatrix.com',
+  }
 }
